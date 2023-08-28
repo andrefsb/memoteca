0cec5fce579885e5d806cd18f63d25ea15f3c378
André Sampaio <andresampaio@id.uff.br>
2023-08-28 20:45:28 -0300

    Listing thoughts

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
+  listaPensamentos = [{
+    id: '1',
+    conteudo: 'Teste1',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  },
+  {
+    id: '2',
+    conteudo: 'Teste2',
+    autoria: 'Dev',
+    modelo: 'modelo2'
+  }];
+
 }
