eab3a2152524994aec853e05f1bffbb7d96057d1
André Sampaio <andresampaio@id.uff.br>
2023-08-28 21:16:56 -0300

    Thought cards styles

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,17 +7,21 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
-  listaPensamentos = [{
-    id: '1',
-    conteudo: 'Teste1',
-    autoria: 'Dev',
-    modelo: 'modelo1'
-  },
+  listaPensamentos = [
   {
-    id: '2',
-    conteudo: 'Teste2',
-    autoria: 'Dev',
+    conteudo: 'Passo informações para o componente filho',
+    autoria: 'Componente pai',
+     modelo: 'modelo3'
+},
+{
+    conteudo: 'Minha propriedade é decorada com @Input()',
+    autoria: 'Componente filho',
     modelo: 'modelo2'
-  }];
+},
+{
+    conteudo: 'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
+    autoria: '',
+    modelo: 'modelo1'
+}];
 
 }
