24549c6b3820966c08e95041127cafda3aa9d394
André Sampaio <andresampaio@id.uff.br>
2023-08-29 14:11:56 -0300

    Select thought as favorite

--- src/app/components/pensamentos/pensamento/pensamento.component.ts
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -1,5 +1,6 @@
+import { Pensamento } from './../pensamento';
 import { Component, Input, OnInit } from '@angular/core';
-import { Pensamento } from '../pensamento.js';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-pensamento',
@@ -9,24 +10,35 @@ import { Pensamento } from '../pensamento.js';
 export class PensamentoComponent implements OnInit {
 
   @Input() pensamento: Pensamento = {
-    id:0,
+    id: 0,
     conteudo: 'I love Angular',
     autoria: 'Nay',
-    modelo: 'modelo3'
-}
-
-constructor(){
-
-}
-
-ngOnInit(): void {
-}
-
-larguraPensamento(): string {
-  if(this.pensamento.conteudo.length >= 256) {
-    return 'pensamento-g'
+    modelo: 'modelo3',
+    favorito: false
   }
-  return 'pensamento-p'
-}
+
+  constructor(private service: PensamentoService) { }
+
+  ngOnInit(): void {
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
+  mudarIconeFavorito(): string {
+    if(this.pensamento.favorito == false) {
+      return 'inativo'
+    }
+    return 'ativo'
+  }
+
+  atualizarFavoritos() {
+    this.service.mudarFavorito(this.pensamento).subscribe();
+    console.log(this.pensamento.favorito)
+  }
 
 }
